30cb68ccef1336b86891a8be9e267f349e53959a
JustTaz <j16020825@student.newinti.edu.my>
2020-09-11 02:33:11 +0800

    Version 2

--- app/src/main/java/com/csci366/july2020/tasfique_enam/labtask2/MainActivity.java
+++ app/src/main/java/com/csci366/july2020/tasfique_enam/labtask2/MainActivity.java
@@ -7,17 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import  java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText userName;
     private EditText password;
     private Button button;
+    private String file = "file"; //check here for the file type.
+    private String file2 = "file2";
+    private String file3 = "file3";
+    private String fileContents = "hello";
+
+    private TextView dateTimeDisplay;
+    private Calendar mCalendar;
+    private SimpleDateFormat mDateFormat;
+    private String date;
+    private boolean t = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +41,17 @@ public class MainActivity extends AppCompatActivity {
         userName = findViewById(R.id.editTextUserID);
         password = findViewById(R.id.editTextPassword);
         button =  findViewById(R.id.button_login);
+        dateTimeDisplay = findViewById(R.id.textViewDate);
+
+        mCalendar = Calendar.getInstance();
+        mDateFormat = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss a");
+        date = mDateFormat.format(mCalendar.getTime());
+        //dateTimeDisplay.setText(date);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                File file = new File("test.txt");
 
                 Intent i = new Intent(MainActivity.this, UserInfo.class);
 
@@ -41,28 +61,67 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "User Name or Password is empty.", Toast.LENGTH_SHORT).show();
                     return;
                 } else if (textUserName.matches("spiderman") || textPassword.matches("man12spider")) {
+
                     try {
-                        PrintWriter output = new PrintWriter(file);
-                        output.println("Taz");
-                    } catch (FileNotFoundException e) {
+
+                        FileOutputStream fOut = openFileOutput(file, MODE_APPEND );
+                        fOut.write(date.getBytes()); //filecontents is a string
+                        fOut.write(System.getProperty("line.separator").getBytes());
+                        fOut.close();
+                        File fileDir = new File(getFilesDir(), file);
+                        Toast.makeText(getBaseContext(), "File Saved" + fileDir, Toast.LENGTH_LONG).show();
+
+                    } catch (Exception e) {
                         e.printStackTrace();
+                        Toast.makeText(getBaseContext(), "Something went Wrong in writing.", Toast.LENGTH_LONG).show();
+                    }
+
+                    i.putExtra("username", textUserName);
+                    i.putExtra("filename", file);
+                    startActivity(i);
+
+                } else if (textUserName.matches("delta") || textPassword.matches("tadel2")) {
+
+                    try {
+
+                        FileOutputStream fOut = openFileOutput(file2, MODE_APPEND );
+                        fOut.write(date.getBytes()); //filecontents is a string
+                        fOut.write(System.getProperty("line.separator").getBytes());
+                        fOut.close();
+                        File fileDir = new File(getFilesDir(), file2);
+                        Toast.makeText(getBaseContext(), "File Saved" + fileDir, Toast.LENGTH_LONG).show();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(getBaseContext(), "Something went Wrong in writing.", Toast.LENGTH_LONG).show();
+                    }
+
+                    i.putExtra("username",textUserName);
+                    i.putExtra("filename", file);
+                    startActivity(i);
+
+                } else if (textUserName.matches("admin") || textPassword.matches("nimda3")) {
+
+                    try {
+                        FileOutputStream fOut = openFileOutput(file3, MODE_PRIVATE);
+                        fOut.write(date.getBytes()); //filecontents is a string
+                        fOut.write(System.getProperty("line.separator").getBytes());
+                        fOut.close();
+                        File fileDir = new File(getFilesDir(), file3);
+                        Toast.makeText(getBaseContext(), "File Saved" + fileDir, Toast.LENGTH_LONG).show();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(getBaseContext(), "Something went Wrong in writing.", Toast.LENGTH_LONG).show();
                     }
 
                     i.putExtra("username",textUserName);
                     startActivity(i);
-                } else if (textUserName.matches("delta") || textPassword.matches("tadel2")) {
-                    i.putExtra("username",textUserName);
-                    startActivity(i);
-                } else if (textUserName.matches("admin") || textPassword.matches("nimda3")) {
-                    i.putExtra("username",textUserName);
-                    startActivity(i);
+
                 } else {
                     Toast.makeText(MainActivity.this, "Wrong User Name or Password", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-
-
             }
         });
     }
--- app/src/main/java/com/csci366/july2020/tasfique_enam/labtask2/UserInfo.java
+++ app/src/main/java/com/csci366/july2020/tasfique_enam/labtask2/UserInfo.java
@@ -1,8 +1,19 @@
 package com.csci366.july2020.tasfique_enam.labtask2;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +26,59 @@ public class UserInfo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+        userName = findViewById(R.id.textView_userName);
+        email = findViewById(R.id.textView_email);
+        lastLogin = findViewById(R.id.textView_login);
+
+        i = getIntent();
+
+        String userNaming = i.getStringExtra("username");
+        String fileName = i.getStringExtra("filename");
+
+        if (userNaming.matches("spiderman") && fileName.matches("file")) {
+
+            userName.setText("Hi " + userNaming);
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            RequestOptions requestOptions = new RequestOptions();
+            requestOptions.placeholder(R.drawable.bg_grey);
+            requestOptions.error(R.drawable.bg_grey);
+
+            Glide.with(UserInfo.this)
+                    .load("https://avatarfiles.alphacoders.com/149/thumb-149117.jpg")
+                    .apply(requestOptions)
+                    .into(imageView);
+
+            email.setText("Email: spiderman@web.com");
+
+            List<String> list;
+
+            try{
+                FileInputStream fIn = openFileInput(fileName);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(fIn));
+                int c;
+                String temp = reader.readLine();
+                list = new ArrayList<String>();
+
+//                while ((c = fIn.read()) != -1) {
+//                    temp = temp + Character.toString((char)c);
+//                    list.add(temp);
+//                }
+
+                while (temp != null) {
+                    list.add(temp);
+                    temp = reader.readLine();
+                    lastLogin.setText("Last Login \n" + list.get(3));
+                }
+
+//                lastLogin.setText("Last Login \n" + list.get(3));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
 
 
     }
